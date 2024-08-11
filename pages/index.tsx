83893cc4c70d02145f67289f704e86e3ea1936d0
zandrij <ajosuesh@gmail.com>
2024-08-11 16:56:27 -0400

    testsss

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import { useGoogleLogin } from "@react-oauth/google";
 
 export default function SignIn() {
   const login = useGoogleLogin({
-    onSuccess: tokenResponse => console.log(tokenResponse),
+    onSuccess: codeResponse => console.log(codeResponse),
+    flow: 'auth-code',
+    redirect_uri: 'http://localhost:3000/auth/google/callback',
+    ux_mode: 'redirect'
   });
 
   return (
