e8b231a29a1804a2db2f37852f912e3d1798431b
zandrij <ajosuesh@gmail.com>
2024-08-11 16:19:56 -0400

    cambios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GoogleOAuthProvider clientId="385759592701-kbmv53o6v33dkg2pjpa5q14oke7472jl.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="361520884393-c6pt58cttt98m8nn8u67m2dgek2qqu3c.apps.googleusercontent.com">
         <Component {...pageProps} />
     </GoogleOAuthProvider>
   )
