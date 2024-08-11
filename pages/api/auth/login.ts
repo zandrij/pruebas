6a1f9948e66084a0300121806516f5c4dd821a77
zandrij <ajosuesh@gmail.com>
2024-08-11 18:18:12 -0400

    test

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -6,7 +6,7 @@ export default async function handler(req: any, res: any) {
             throw new Error('GOOGLE_CLIENT_ID is not set in environment variables');
         }
 
-        const redirectUri = 'http://localhost:3000/api/auth/callback';
+        const redirectUri = 'https://pruebas-bice-eight.vercel.app/api/auth/callback';
         const scope = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
         const responseType = 'code';
         const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}&access_type=offline`;
