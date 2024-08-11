95014f5212ced6fc973b8db996950f4990ba07ef
zandrij <ajosuesh@gmail.com>
2024-08-11 18:24:05 -0400

    final

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,9 @@
-'use client';
-import { useGoogleLogin } from "@react-oauth/google";
+"use client";
 
 export default function SignIn() {
-  const login = useGoogleLogin({
-    onSuccess: codeResponse => console.log(codeResponse),
-    flow: 'auth-code',
-    redirect_uri: 'http://localhost:3000/auth/google/callback',
-    ux_mode: 'redirect'
-  });
-
   return (
     <>
-      <button
-        onClick={() => login()}
-      >
-        Sign In
-      </button>
+      <a href="/api/auth/login">Sign In</a>
     </>
   );
 }
